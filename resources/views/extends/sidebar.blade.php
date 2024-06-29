65c1603bef9683f2907afb19e9ab3deccc175860
kanebassirou <kaneb1927@gmail.com>
2024-06-29 13:50:27 +0000

    sidebar

--- resources/views/extends/sidebar.blade.php
+++ resources/views/extends/sidebar.blade.php
@@ -12,7 +12,7 @@
 
         <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">GestSchool</span>
+        <span class="brand-text font-weight-light">CahierTexteNum</span>
 
     </a>
 
